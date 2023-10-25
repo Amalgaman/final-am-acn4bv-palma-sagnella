d2ebf96e81c0a16a39f824529ae7265b620639a5
Amalgaman <xxxrigobertoxxx@gmail.com>
2023-10-25 14:26:46 -0300

    feat: Formulario en Forma de Message dialog

--- app/src/main/java/com/example/crecimonstruo/MainActivity.java
+++ app/src/main/java/com/example/crecimonstruo/MainActivity.java
@@ -12,7 +12,11 @@ import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -25,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvNivel;
     private TextView tvExp;
     private Button bEditarNombre;
-
+    private Button bAnadir;
+    private ImageView imgMonster;
     private Monster mA;
 
     private LinkedList<Task> tasks;
@@ -55,34 +60,49 @@ public class MainActivity extends AppCompatActivity {
         tvNombre = (TextView)findViewById(R.id.tvNombre);
         tvNivel = (TextView)findViewById(R.id.tvNivel);
         tvExp = (TextView)findViewById(R.id.tvExp);
+        imgMonster = (ImageView)findViewById(R.id.imageMonster);
 
         bEditarNombre = (Button)findViewById(R.id.b_editar_nombre);
         bEditarNombre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,EditarNombre.class);
-                startActivity(intent);
+                showInputDialogNombre();
+            }
+        });
+
+        bAnadir = (Button)findViewById(R.id.buttonAnadir);
+        bAnadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFormDialogAnadir();
             }
         });
 
         actualizarMonster();
-        actualizarLista(0);
+        actualizarLista();
 
     }
 
     public void actualizarMonster(){
         tvNombre.setText(mA.getNombre());
         tvNivel.setText("Nivel: "+mA.getNivel());
-        tvExp.setText("Exp: "+mA.getExp()+"/"+5*mA.getNivel());
+        if (mA.getNivel() < 4){
+            tvExp.setText("Exp: "+mA.getExp()+"/"+5*mA.getNivel());
+        }else{
+            tvExp.setText("Exp: "+mA.getExp());
+        }
+
+        int lvl = mA.getNivel()-1;//Para traer la imagen por id
+        imgMonster.setImageResource(getResources().getIdentifier(mA.getImg()[lvl], "drawable",getPackageName()));
     }
 
-    public void actualizarLista(int flag){
+    public void actualizarLista(){
 
         LinearLayout layout = (LinearLayout)findViewById(R.id.listaLayout);
 
-        if(flag == 1){
-            layout.removeAllViews();
-        }
+
+        layout.removeAllViews();
+
 
         Button nuevo;
 
@@ -152,9 +172,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void completarTarea(int id){
-        tasks.get(id-1).setLista(true);
+        Task task = tasks.get(id-1);
+        task.setLista(true);
+        mA.subirNivel(task.getDificultad());
 
-        actualizarLista(1);
+        actualizarMonster();
+        actualizarLista();
     }
 
     private void showConfirmDialogTarea(String titulo, int idTask) { //Crea un mensaje de confirmacion emergente
@@ -171,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
+                        dialog.cancel();
                     }
                 });
 
@@ -179,7 +202,90 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    public void editarNombre(View view){
+    private void showInputDialogNombre() {
+        // Crea el cuadro de diálogo de entrada
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ingrese nuevo Nombre");
 
+        // Configura el campo de entrada
+        final EditText input = new EditText(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT); // Cambiado a WRAP_CONTENT para la altura
+        input.setLayoutParams(lp);
+        builder.setView(input);
+
+        // Configura los botones de aceptar y cancelar
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String userInput = input.getText().toString();
+                if(!userInput.equals("")){
+                    mA.setNombre(userInput);
+                    actualizarMonster();
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Muestra el cuadro de diálogo
+        builder.show();
+    }
+
+    private void showFormDialogAnadir() {
+        // Crea el cuadro de diálogo de formulario
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Inflar el diseño del formulario dentro del cuadro de diálogo
+        View formView = getLayoutInflater().inflate(R.layout.activity_editar_nombre, null);
+        builder.setView(formView);
+
+        // Configurar los botones de aceptar y cancelar
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Aquí puedes obtener los valores de los campos del formulario
+                EditText editText1 = formView.findViewById(R.id.editTitulo);
+                RadioGroup radioGroup = formView.findViewById(R.id.radio_group);
+                RadioButton rb1 = formView.findViewById(R.id.radio_button1);
+                RadioButton rb2 = formView.findViewById(R.id.radio_button2);
+                RadioButton rb3 = formView.findViewById(R.id.radio_button3);
+
+                String titulo = editText1.getText().toString();
+                int seleccion = 0;
+
+                if (rb1.isChecked()) {
+                    seleccion = 1;
+                } else if (rb2.isChecked()) {
+                    seleccion = 2;
+                } else if (rb3.isChecked()) {
+                    seleccion = 3;
+                }
+
+                if(seleccion > 0 && !titulo.equals("")){
+                    tasks.add(new Task(titulo, "Poner la ropa en el lavarropa y colgarla", seleccion, new Date()));
+                    actualizarLista();
+                }
+
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Mostrar el cuadro de diálogo del formulario
+        builder.show();
     }
 }
+
+
